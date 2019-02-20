3220c6730a8564dec4da0ca129f72f947959d172
developer <g.eduardcatalin.dev@gmail.com>
2019-02-20 20:54:13 +0200

    updated the qrcode from clipboard script

--- qrcode_clipboard.sh
+++ qrcode_clipboard.sh
@@ -3,7 +3,5 @@
 CLIPBOARD=$(xclip -o -selection clipboard)
 IMAGE_PATH=~/Desktop/qrcode.png
 
-qrcode -o $IMAGE_PATH "$CLIPBOARD" && eog $IMAGE_PATH
-
-rm $IMAGE_PATH
+qrcode -o $IMAGE_PATH "$CLIPBOARD" && eog $IMAGE_PATH && rm $IMAGE_PATH
 
