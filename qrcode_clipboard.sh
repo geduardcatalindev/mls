d4d42d2e9d0325ed00d270a9ee9c39ac7ec5096d
developer <g.eduardcatalin.dev@gmail.com>
2019-02-20 21:59:18 +0200

    moved the temporary image created by qrcode in /tmp

--- qrcode_clipboard.sh
+++ qrcode_clipboard.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 CLIPBOARD=$(xclip -o -selection clipboard)
-IMAGE_PATH=~/Desktop/qrcode.png
+IMAGE_PATH=/tmp/qrcode_clipboard.png
 
 qrcode -o $IMAGE_PATH "$CLIPBOARD" && eog $IMAGE_PATH && rm $IMAGE_PATH
 
